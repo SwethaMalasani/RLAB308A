1ac51b5ea0504ffe3204b6802f900e9cc67785ce
SwethaMalasani <malasaniswetha520@gmail.com>
2025-01-10 14:27:48 -0500

    cmmitted

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const getFavouritesBtn = document.getElementById("getFavouritesBtn");
 const API_KEY = 'live_w0Ezw2vCZfWAMsTIAn4kkFfqZpvrrOlZOc682APaaI8IijxazuV1ArD8fmeTvIkF';
 
 // Function to update the progress bar during an API request
+/**6.Create a progress bar to indicate the request is in progress.
+The progressBar element has already been created for you.
+You need only to modify its width style property to align with the request progress.
+In your request interceptor, set the width of the progressBar element to 0%.
+This is to reset the progress with each request.
+Research the axios onDownloadProgress config option.
+Create a function "updateProgress" that receives a ProgressEvent object.
+Pass this function to the axios onDownloadProgress config option in your event handler.
+console.log your ProgressEvent object within updateProgress, and familiarize yourself with its structure.
+Update the progress of the request using the properties you are given.
+Note that we are not downloading a lot of data, so onDownloadProgress will likely only 
+fire once or twice per request to this API. This is still a concept worth familiarizing yourself with for future projects.*/
 function updateProgress(progressEvent) {
   console.log(progressEvent);
 
@@ -19,7 +31,13 @@ function updateProgress(progressEvent) {
     progressBar.style.width = `${percent}%`;
   }
 }
-
+/**Within this additional file, change all of your fetch() functions to Axios!
+Axios has already been imported for you within index.js.
+If you've done everything correctly up to this point, this should be simple.
+If it is not simple, take a moment to re-evaluate your original code.
+Hint: Axios has the ability to set default headers. Use this to your advantage by setting a default header 
+with your API key so that you do 
+not have to send it manually with all of your requests! You can also set a default base URL! */
 // Initialize the breed selection dropdown by fetching breed data
 async function initialLoad() {
   const req = await axios.get("https://api.thecatapi.com/v1/breeds", {
@@ -69,7 +87,10 @@ breedSelect.addEventListener("change", async (e) => {
   // Start the carousel functionality
   Carousel.start();
 });
-
+/**5.Add Axios interceptors to log the time between request and response to the console.
+Hint: you already have access to code that does this!
+Add a console.log statement to indicate when requests begin.
+As an added challenge, try to do this on your own without referencing the lesson material. */
 // Axios interceptors for logging request/response time
 axios.interceptors.request.use(request => {
   progressBar.style.width = "0%"; // Reset progress bar on new request
@@ -93,6 +114,14 @@ axios.interceptors.response.use(response => {
 });
 
 // Function to handle toggling favourite status for an image
+/**To practice posting data, we will create a system to "favourite" certain images.
+The skeleton of this favourite() function has already been created for you.
+This function is used within Carousel.js to add the event listener as items are created.
+This is why we use the export keyword for this function.
+Post to the cat API's favourites endpoint with the given id.
+The API documentation gives examples of this functionality using fetch(); use Axios!
+Add additional logic to this function such that if the image is already favourited, you delete that favourite using the API, giving this function "toggle" behavior.
+You can call this function by clicking on the heart at the top right of any image. */
 export async function favourite(imgId) {
   try {
     // Get all favourites
@@ -126,7 +155,12 @@ export async function favourite(imgId) {
     alert('An error occurred while favouriting the image');
   }
 }
-
+/**Test your favourite() function by creating a getFavourites() function.
+Use Axios to get all of your favourites from the cat API.
+Clear the carousel and display your favourites when the button is clicked.
+You will have to bind this event listener to getFavouritesBtn yourself.
+Hint: you already have all of the logic built for building a carousel. If that is not in its own function, 
+maybe it should be so that you do not have to repeat yourself in this section. */
 // Function to fetch and display the user's favourites
 async function getFavourites() {
   try {
